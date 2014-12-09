70b298523a5500d9bd0880cefb0d486e7c85db74
k-totani <syshayr_soccer_46@yahoo.co.jp>
2014-12-09 13:01:49 +0900

    こちらがfirst commitです。

---
+++ Tetoris/constData.m
@@ -0,0 +1,14 @@
+//
+//  constData.m
+//  Tetoris
+//
+//  Created by totta on 2014/12/04.
+//  Copyright (c) 2014年 totta. All rights reserved.
+//
+
+#import "constData.h"
+
+@implementation constData
+
+
+@end
